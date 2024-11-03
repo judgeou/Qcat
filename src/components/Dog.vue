0f7a75dc1f06894deb722670d1b81c263070d56d
judgeou <judgeou@gamil.com>
2024-11-04 01:29:18 +0800

    添加发送涩图功能

--- src/components/Dog.vue
+++ src/components/Dog.vue
@@ -21,9 +21,15 @@ const group_id = ref('')
 const user_id = ref('')
 const isRecording = ref(false)
 const blob_url = ref('')
-
+const image_text = ref('')
+const image_file = ref<File | null>(null)
+const is_merge_forward = ref(false)
 let mediaRecorder: MediaRecorder;
 let audioBlob: Blob;
+
+function random_id (min = 100000000, max = 900000000) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
 async function onebot_call (action = 'send_group_msg', params: any) {
   info.value = 'sending...'
@@ -54,6 +60,34 @@ async function send_group_record (file: any) {
       }
     ]
   })
+}
+
+async function send_group_image (dataurl: string) {
+  let messages = []
+  if (is_merge_forward.value) {
+    const content = []
+    if (image_text.value) {
+      content.push({ "type": "text", "data": { "text": image_text.value } })
+    }
+    content.push({ "type": "image", "data": { "file": dataurl } })
+    messages = [{ "type": "node", "data": {
+      "user_id": random_id().toString(),
+      "nickname": "QQ 用户",
+      "content": content
+    } }]
+
+    await onebot_call('send_forward_msg', {
+      group_id: group_id.value,
+      message: messages
+    })
+  } else {
+    messages = [{ "type": "image", "data": { "file": dataurl } }]
+
+    await onebot_call('send_group_msg', {
+      group_id: group_id.value,
+      message: messages
+    })
+  }
 }
 
 async function send_user_record (file: any) {
@@ -114,7 +148,7 @@ async function send_record_to_user () {
   await send_private_msg(dataurl)
 }
 
-async function blob_to_dataurl (blob: Blob) : Promise<string> {
+async function blob_to_dataurl (blob: Blob | File) : Promise<string> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader()
     reader.onload = () => resolve(reader.result as string)
@@ -122,26 +156,40 @@ async function blob_to_dataurl (blob: Blob) : Promise<string> {
     reader.readAsDataURL(blob)
   })
 }
+
+function onFileChange (event: Event) {
+  const files = (event.target as HTMLInputElement).files
+  if (files && files.length > 0) {
+    image_file.value = files[0]
+  }
+}
+
+async function send_image_to_group () {
+  if (image_file.value) {
+    const dataurl = await blob_to_dataurl(image_file.value)
+    await send_group_image(dataurl)
+  }
+}
 </script>
 
 <template>
-  <h1>Hello</h1>
-  <div>
+  <h1>Qcat</h1>
+  <h2>兼容 <a href="https://napcat.napneko.icu/" target="_blank">NapCatQQ</a></h2>
+  <div class="row">
     <input type="text" v-model="http_port" placeholder="HTTP 服务端口">
   </div>
-  <div>
+  <div class="row">
     <input type="text" v-model="group_id" placeholder="QQ 群号">
     <input type="text" v-model="user_id" placeholder="QQ 用户号">
-    <input type="text" v-model="record_file" placeholder="音频文件本地路径">
   </div>
 
-  <div>
-    <!-- <button @click="send_group_msg_text('你好')">send_group_msg_text</button> -->
+  <div class="row">
+    <input type="text" v-model="record_file" placeholder="音频文件本地路径">
     <button @click="send_group_record(record_file)">发送音频文件到群</button>
     <button @click="send_user_record(record_file)">发送音频文件到用户</button>
   </div>
 
-  <div>
+  <div class="row">
     <button @click="send_record()" v-if="!isRecording">开始录音</button>
     <button @click="stopRecording()" v-if="isRecording">停止录音</button>
     <audio :src="blob_url" controls v-if="blob_url"></audio>
@@ -149,6 +197,12 @@ async function blob_to_dataurl (blob: Blob) : Promise<string> {
     <button v-if="blob_url" @click="send_record_to_user()">发送到用户</button>
   </div>
 
+  <div class="row">
+    <input type="file" @change="onFileChange">
+    <!-- <input type="text" v-model="image_text" placeholder="涩图描述"> -->
+    <input type="checkbox" v-model="is_merge_forward"> 合并转发
+    <button @click="send_image_to_group()">发送涩图到群</button>
+  </div>
 
   <p>
     {{ info }}
@@ -156,5 +210,14 @@ async function blob_to_dataurl (blob: Blob) : Promise<string> {
 </template>
 
 <style scoped>
-
+.row {
+  display: flex;
+  gap: 12px;
+  margin-top: 12px;
+  align-items: center;
+}
+input[type="checkbox"] {
+  width: 16px;
+  height: 16px;
+}
 </style>
